1fd839d86e8707de240e1083e993e0c069385976
Solus Dev <Solus_Devl@Solustest.com>
2018-09-23 00:54:59 -0400

    fakecoin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -6,6 +6,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 // alsdfnkljadsfl
+// ajsdfnasdfjlasdf
 #include "chainparams.h"
 
 #include "random.h"
