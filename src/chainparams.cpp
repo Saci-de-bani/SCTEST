dcc68ff7dcc870268d8278a3081b44819b14d24d
Solus Dev <Solus_Devl@Solustest.com>
2018-09-23 00:48:04 -0400

    martini bikiki

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -5,7 +5,7 @@
 // Copyright (c) 2018 The Solar developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+// alsdfnkljadsfl
 #include "chainparams.h"
 
 #include "random.h"
